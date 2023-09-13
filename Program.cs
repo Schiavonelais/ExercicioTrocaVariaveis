fb561e5bd777cefd4fa0e474af9410b9f5b1a997
Lais Schiavone Sobral <202304133441@alunos.estacio.br>
2023-09-13 09:29:12 -0700

    Exercicio Completo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string PrimeiroNome, SegundoNome;
+
+Console.Write("Escreva seu Nome:");
+PrimeiroNome=Console.ReadLine();
+
+Console.Write("Escreva seu Nome:");
+SegundoNome=Console.ReadLine();
+
+Console.WriteLine("-------Troca de Valores -------");
+Console.WriteLine($"{PrimeiroNome}, {SegundoNome}");
+Console.WriteLine("-Após a Troca de Valores, Temos:");
+Console.WriteLine($"{SegundoNome}, {PrimeiroNome}");
